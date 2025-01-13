a00c68ad41c089fcb3046efadd529b93c1a3feaa
Andres Guerrero <aguerrero@fi.uba.ar>
2025-01-13 00:02:28 -0300

    add env

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { CalendarPage } from '../calendar'
+import { getEnvVariables } from '../helpers'
 
 
 export const AppRouter = () => {
+
     const authStatus = 'authenticated'
+
     return (
         <Routes>
             {
